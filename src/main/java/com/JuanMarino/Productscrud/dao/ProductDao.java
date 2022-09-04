43ff81a74a06abf2343be91701450bacdea22227
JuanCarlosMarino <juan.marino.morantes@gmail.com>
2022-09-04 08:51:32 -0500

    Product entity and Dao

---
+++ src/main/java/com/JuanMarino/Productscrud/dao/ProductDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.JuanMarino.Productscrud.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface ProductDao extends CrudRepository<ProductDao, Integer>{
+    
+}
---
+++ src/main/java/com/JuanMarino/Productscrud/models/ProductEntity.java
@@ -0,0 +1,74 @@
+
+package com.JuanMarino.Productscrud.models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "producto")
+public class ProductEntity implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idProducto")
+    private Integer idProduct;
+    
+    @Column(name = "nombreProducto")
+    private String productName;
+    
+    @Column(name = "valorCompra")
+    private double purchaseValue;
+    
+    @Column(name = "valorVenta")
+    private double saleValue;
+    
+    @Column(name = "cantidad")
+    private double amount;
+
+    public Integer getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Integer idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPurchaseValue() {
+        return purchaseValue;
+    }
+
+    public void setPurchaseValue(double purchaseValue) {
+        this.purchaseValue = purchaseValue;
+    }
+
+    public double getSaleValue() {
+        return saleValue;
+    }
+
+    public void setSaleValue(double saleValue) {
+        this.saleValue = saleValue;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    
+    
+}
