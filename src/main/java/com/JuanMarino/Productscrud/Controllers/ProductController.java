38cd5f9e893f4e4025f5eaa591307a923f827bc7
JuanCarlosMarino <juan.marino.morantes@gmail.com>
2022-09-04 11:09:50 -0500

    add, delete and update methods

--- src/main/java/com/JuanMarino/Productscrud/Controllers/ProductController.java
+++ src/main/java/com/JuanMarino/Productscrud/Controllers/ProductController.java
@@ -8,9 +8,15 @@ import com.JuanMarino.Productscrud.models.ProductEntity;
 import com.JuanMarino.Productscrud.services.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,16 +29,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
-    private ProductService servicioProducto;
+    private ProductService serviceProducto;
     
-    @GetMapping("/list")
-    public List<ProductEntity> consultarTodo(){
-        return servicioProducto.findAll();
+    @GetMapping("/")
+    public List<ProductEntity> consultAll(){
+        return serviceProducto.findAll();
     }
     
     @GetMapping("/{id}")
-    public ProductEntity consultarPorId(@PathVariable Integer id){
-        return servicioProducto.findById(id);
+    public ProductEntity consultById(@PathVariable Integer id){
+        return serviceProducto.findById(id);
     }
     
+    @PostMapping("/")
+    public ResponseEntity<ProductEntity> add(@RequestBody ProductEntity product){
+        ProductEntity record = serviceProducto.save(product);
+        return new ResponseEntity<>(record, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductEntity> delete(@PathVariable Integer id){
+        ProductEntity foundRecord = serviceProducto.findById(id);
+        if (foundRecord != null){
+            serviceProducto.delete(id);
+        }
+        return new ResponseEntity<>(foundRecord, HttpStatus.OK);
+    }
+    
+    @PutMapping("/")
+    public ResponseEntity<ProductEntity> update(@RequestBody ProductEntity product){
+        ProductEntity foundRecord = serviceProducto.findById(product.getIdProduct());
+        if(foundRecord!=null){
+            foundRecord.setProductName(product.getProductName());
+            foundRecord.setPurchaseValue(product.getPurchaseValue());
+            foundRecord.setSaleValue(product.getSaleValue());
+            foundRecord.setAmount(product.getAmount());
+            serviceProducto.save(product);
+        }
+        return new ResponseEntity<>(foundRecord, HttpStatus.OK);
+    }
 }
