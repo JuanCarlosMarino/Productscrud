1b1a821cffcce7a107bec2060fd87651831716a7
JuanCarlosMarino <juan.marino.morantes@gmail.com>
2022-09-04 11:16:40 -0500

    Change database

---
+++ database/storeDB.sql
@@ -0,0 +1,32 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema store
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema store
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `store` DEFAULT CHARACTER SET utf8 ;
+USE `store` ;
+
+-- -----------------------------------------------------
+-- Table `store`.`producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `store`.`producto` (
+  `idproducto` INT NOT NULL AUTO_INCREMENT,
+  `nombreproducto` VARCHAR(45) NOT NULL,
+  `valorcompra` DOUBLE NOT NULL,
+  `valorventa` DOUBLE NOT NULL,
+  `cantidad` DOUBLE NOT NULL,
+  PRIMARY KEY (`idproducto`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
