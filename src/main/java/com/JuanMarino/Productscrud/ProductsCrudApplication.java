441e08347016e9ca15f6aa063a08e5187be35421
Juan Mariño <misiontic.tutor18@uis.edu.co>
2022-09-04 08:15:11 -0500

    Initial commit and project with spring initializer

---
+++ src/main/java/com/JuanMarino/Productscrud/ProductsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.JuanMarino.Productscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JuanMarino/Productscrud/ProductsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuanMarino.Productscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
