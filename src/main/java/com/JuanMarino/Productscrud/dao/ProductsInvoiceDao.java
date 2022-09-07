684f8f43ab929de16e6b79d1debca03eea552998
JuanCarlosMarino <juan.marino.morantes@gmail.com>
2022-09-07 08:39:29 -0500

    Data access Objects created

---
+++ src/main/java/com/JuanMarino/Productscrud/dao/ProductsInvoiceDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.JuanMarino.Productscrud.dao;
+
+import com.JuanMarino.Productscrud.models.ProductsInvoiceEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface ProductsInvoiceDao extends CrudRepository<ProductsInvoiceEntity, Integer>{
+    
+}
